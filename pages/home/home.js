9ab16a7d0c12787c1b35fc911ccc96341a90e02d
lillizhixiong <1260672840@qq.com>
2020-03-18 00:45:30 +0800

    小程序的初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    Name: "lizhixiong",
+    age: 18,
+    students:[
+      {id: 110, name: 'kobe', age: 30},
+      {id: 111, name: 'james', age: 31},
+      {id: 112, name: 'curry', age: 28},
+      {id: 113, name: 'tom', age: 30}
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  add() {
+    let _this = this;
+    _this.setData({
+      counter: _this.data.counter + 1
+    })
   },
+  sub() {
+    let _this = this;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    _this.setData({
+      counter: _this.data.counter - 1
+    })
   }
 })
