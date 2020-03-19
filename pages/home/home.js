1f9da0fea96ded7f1601e1bc9b41896f449bce8e
lillizhixiong <1260672840@qq.com>
2020-03-19 21:58:24 +0800

    06_页面注册和页面的生命周期函数onLoad,onShow,onHide,onReady

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,55 @@
 // pages/home/home.js
+
+// 调用Page方法注册一个页面
+//每个页面也有自己的生命周期函数
 Page({
+  // -----------2.初始化数据 -------------
+  data:{
+    list:[]
+  },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  //--------1. 监听页面的生命周期函数--------
+  // 运行顺序1
+  // 页面被加载出来
+  onLoad() {
+    // 一般在这里发送网络请求
+    wx.request({
+      url: '',
+      success:function() {
+
+      }
+    })
+  },
+
+  // 运行顺序2  执行多次
+  // 页面显示出来时
+  onshow(){
+
+  },
+  // 页面隐藏后执行
+  onHide() {},
+
+  // 运行顺序3
+  // 小程序页面再初次渲染完成 (只执行一次)
+  onReady() {
+
+  },
+  //页面被卸载卸载后,就是跳转以后
+  onUnload() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // ------3.监听XML里面的一些事件 ----------
 
+  // ------4.监听其他事件 ------------
+  // 监听页面的滚动
+  onPageScroll(obj) {
+    console.log(obj)
   },
+  // 监听页面滚动动到底部
+  onReachBottom() {},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 下拉刷新(在json文件开启true))
+  onPullDownRefresh() {}
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
