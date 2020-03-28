66fe3ee7a1f410bb881ac7c10decddb2dca851ad
lillizhixiong <1260672840@qq.com>
2020-03-28 14:26:11 +0800

    直接获取组件对象this_selectComponent_类名,id名

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,7 +7,15 @@ Page({
   data: {
 
   },
+  handleAddCpn(){
+    // 最终目的是:修改my-sel的counter计算
+    // 组件对象.setData({counter:})
 
+    // const my_sel = this.selectComponent('class/id')
+    const my_sel = this.selectComponent('#sel-id')
+    // 直接调用组件内部的方法 或者让组件内部发送事件出来
+    my_sel.handleAddClick()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
